e239755a1f0b07e8b2f9a9fc0e4d3a37b24d3fa7
Daniel Seita <takeshidanny@gmail.com>
2016-11-11 23:03:47 -0800

    More linked lists. Having fun with these

---
+++ 02_Linked_Lists/hash_table.py
@@ -0,0 +1,135 @@
+"""
+Test implementation of Hash Tables which use linked lists (avoids problems with
+arrays of adding/removing which requires shifting elements, and also
+re-allocating memory if array is full). Try to minimize external libraries, i.e.
+use plain Python.
+"""
+import sys
+
+class SListNode:
+    """ Singly-linked Node, references to any object. """
+
+    def __init__(self, i, n=None):
+        self._item = i
+        self._next = n
+
+
+class SList:
+    """ A class whose job is to maintain the first node of an SListNode.
+    Enforces the invariants that the size is always correct, and the list is
+    never circularly linked.
+    """
+
+    def __init__(self, h=None):
+        self._head = h
+        self._size = 0
+
+    def insert_front(self, item):
+        self._head = SListNode(item, self._head)
+        self._size += 1
+
+    def to_string(self):
+        if self._size == 0:
+            return "[head] -> [None]"
+        l_str = "[head] -> " + self._head._item
+        node = self._head
+        while node._next != None:
+            node = node._next
+            l_str += " -> " + node._item
+        return l_str + " -> [None]"
+
+
+class DListNode:
+    """ Doubly-linked Node, references to any object. """
+
+    def __init__(self, i, n=None, p=None):
+        self._item = i
+        self._next = n
+        self._prev = p
+
+
+class DList:
+    """ Similar to SList, except for the doubly-linked version. This points to
+    the `sentinel` which does not represent an item. To detect if we are at the
+    Sentinel, check that the item is equal to SENTINEL (a special item that we
+    assume we never have in our real items). """
+
+    def __init__(self):
+        self._sentinel = DListNode("SENTINEL", n=None, p=None)
+        self._sentinel._next = self._sentinel
+        self._sentinel._prev = self._sentinel
+        self._size = 0
+
+    def insert_front(self, item):
+        if self._size == 0:
+            d = DListNode(item, n=self._sentinel, p=self._sentinel)
+            self._sentinel._next = d
+            self._sentinel._prev = d
+        else:
+            d = DListNode(item, n=self._sentinel._next, p=self._sentinel)
+            self._sentinel._next._prev = d
+            self._sentinel._next = d
+        self._size += 1
+
+    def remove_last(self):
+        if (self._sentinel._prev != self._sentinel):
+            # The below is correct, but simpler to do it other way around.
+            #self._sentinel._prev._prev._next = self._sentinel
+            #self._sentinel._prev = self._sentinel._prev._prev
+            # Reversed (though one statement is exactly the same):
+            self._sentinel._prev = self._sentinel._prev._prev
+            self._sentinel._prev._next = self._sentinel
+            self._size -= 1
+
+    def to_string(self):
+        if self._size == 0:
+            return "[head] -> [sentinel]"
+        node = self._sentinel._next
+        l_str = "[head] -> [sentinel] -> " + node._item
+        while node._next._item != "SENTINEL":
+            node = node._next
+            l_str += " -> " + node._item
+        return l_str + " -> [sentinel]"
+
+
+# Now let's practice and test things out!
+if __name__ == "__main__":
+
+    # SLists
+    print("Debugging with SLists:")
+    x = SList()
+    y = SList()
+    y.insert_front("blue")
+    y.insert_front("rasp")
+
+    print(x.to_string())
+    x.insert_front("milk")
+    print(x.to_string())
+    x.insert_front("fish")
+    print(x.to_string())
+    print(y.to_string())
+
+    # DLists
+    print("\nNow on DLists:")
+    d = DList()
+    print(d.to_string())
+    d.insert_front("obama")
+    print(d.to_string())
+    d.insert_front("bush43")
+    print(d.to_string())
+    d.insert_front("clinton")
+    print(d.to_string())
+    d.insert_front("bush41")
+    print(d.to_string())
+    d.remove_last()
+    print(d.to_string())
+    d.remove_last()
+    print(d.to_string())
+    d.remove_last()
+    print(d.to_string())
+    d.remove_last()
+    print(d.to_string())
+    d.remove_last()
+    print(d.to_string())
+    d.insert_front("obama")
+    print(d.to_string())
--- 02_Linked_Lists/questions.py
+++ 02_Linked_Lists/questions.py
@@ -2,6 +2,49 @@
 Linked list questions
 """
 
+from hash_table import *
+
+def question_01(slist):
+    """ Assumes 'slist' is of type SList. Removes duplicates but no buffer
+    allowed. """
+
+    if slist._size <= 1:
+        return
+    s1 = slist._head
+    s2 = slist._head
+
+    while (s1 != None):
+        while (s2._next != None):
+            if (s1._item == s2._next._item):
+                s2._next = s2._next._next
+            else:
+                s2 = s2._next
+        s1 = s1._next
+        s2 = s1
 
 if __name__ == "__main__":
-    pass
+    x = SList()
+    question_01(x)
+    print(x.to_string())
+
+    x.insert_front("fish")
+    question_01(x)
+    print(x.to_string())
+
+    x.insert_front("fish")
+    print(x.to_string())
+    question_01(x)
+    print(x.to_string())
+
+    y = SList()
+    y.insert_front("hi")
+    y.insert_front("hi")
+    y.insert_front("bye")
+    y.insert_front("no")
+    y.insert_front("hi")
+    y.insert_front("bye")
+    y.insert_front("what")
+    y.insert_front("hi")
+    print(y.to_string())
+    question_01(y)
+    print(y.to_string())
